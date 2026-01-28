f89f082ff2f342e162918bee0202c06d4dbce163
Mohammed Al-Haifi <mohammed.al-haifi@sait.ca>
2026-01-28 13:39:17 -0700

    insta clone

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import LandingPage from "@/components/landing-page";
+
+export default function Index() {
+  return (
+    <View>
+      <LandingPage />
+    </View>
+  );
+}
---
+++ components/landing-page.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <View>
+      <Text>landing-page</Text>
+    </View>
+  )
+}
+
+export default LandingPage
+
+const styles = StyleSheet.create({})
